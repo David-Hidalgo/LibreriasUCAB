9cfa940336f94e62317bae2b86a78e506ce88f75
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-05-07 18:03:28 -0400

    feat: :tada: Inicio para las librerias
    suerte gente

---
+++ sonNumeros.h
@@ -0,0 +1,13 @@
+#ifndef SONNUMEROS_H
+#define SONNUMEROS_H
+
+#include <string>
+using namespace std;
+
+//función para saber si un string es un número entero
+bool isInteger(string str);
+//función para saber si son solo espacios
+bool soloEspacios(string str);
+
+
+#endif
